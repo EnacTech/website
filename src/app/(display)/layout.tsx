548f33ea1b805b0b4506c068fa2fcde9a7ae9d4c
madhav <eragon.cube@outlook.com>
2024-01-27 00:22:13 +0530

    Refactor layout by separating admin and public routes

--- src/app/(display)/layout.tsx
+++ src/app/(display)/layout.tsx
@@ -2,13 +2,13 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import MobileMenu from '@/components/MobileMenu';
 import Header from '@/components/Header';
-import './globals.css';
+import '../globals.css';
 import Footer from '@/components/Footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Home | Enactus NSUT',
+  title: 'Enactus NSUT',
   description:
     'Enactus NSUT is a community of students, academics and business leaders committed to using the power of entrepreneurial action to transform lives and shape a better and more sustainable world.',
 };
